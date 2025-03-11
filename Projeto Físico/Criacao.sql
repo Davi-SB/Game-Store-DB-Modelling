125736737ba2c299e4d9584a26d2c4aa3b436ff6
Davi <davibrilhante0102@gmail.com>
2025-03-10 21:22:25 -0300

    fix projeto físico

--- Projeto Físico/Criacao.sql
+++ Projeto Físico/Criacao.sql
@@ -1,5 +1,7 @@
--- (dsb6) NÃO RODEI AINDA, PODE TER ERROS
--- (dsb6) adicionei umas restrições de atributos NOT NULL que não faz parte do escopo do projeto lógico, mas faz sentido pras tabelas do lógico. Fiz isso pq Robson fez isso tbm nos slides de SQL quando foi passar do lógico pro físico. Essas mudanças tão registradas no arquivo "rascunho.sql".
+-- (dsb6) adicionei umas restrições de atributos NOT NULL que não fazem parte do escopo 
+-- do projeto lógico, mas fazem sentido pras tabelas do lógico. Fiz isso pq Robson fez 
+-- isso tbm nos slides de SQL quando foi passar do lógico pro físico. Essas mudanças 
+-- tão registradas no arquivo "rascunho.sql".
 
 -- Tabela Usuario
 CREATE TABLE Usuario (
@@ -9,8 +11,8 @@ CREATE TABLE Usuario (
 
 -- Tabela Amigo
 CREATE TABLE Amigo (
-    Amigo1 INT NOT NULL,
-    Amigo2 INT NOT NULL,
+    Amigo1 INT,
+    Amigo2 INT,
     PRIMARY KEY (Amigo1, Amigo2),
     FOREIGN KEY (Amigo1) REFERENCES Usuario(ID),
     FOREIGN KEY (Amigo2) REFERENCES Usuario(ID)
@@ -19,18 +21,17 @@ CREATE TABLE Amigo (
 -- Tabela Conta
 CREATE TABLE Conta (
     ID INT PRIMARY KEY,
-    Senha VARCHAR(255) NOT NULL,
-    Email VARCHAR(255) NOT NULL,
-    Cartao VARCHAR(50),
+    Senha VARCHAR(100) NOT NULL,
+    Email VARCHAR(100) NOT NULL,
     FOREIGN KEY (ID) REFERENCES Usuario(ID)
 );
 
 -- Tabela Cartao
 CREATE TABLE Cartao (
-    ID INT NOT NULL,
-    Num VARCHAR(20) NOT NULL,
-    Cod INT NOT NULL,
-    DT_Exp DATE NOT NULL,
+    ID INT,
+    Num VARCHAR(20),
+    Cod INT,
+    DT_Exp DATE,
     PRIMARY KEY (ID, Num, Cod, DT_Exp),
     FOREIGN KEY (ID) REFERENCES Conta(ID)
 );
@@ -45,11 +46,11 @@ CREATE TABLE Jogo (
 
 -- Tabela Comentario
 CREATE TABLE Comentario (
-    Jogo_ID INT NOT NULL,
-    Usuario_ID INT NOT NULL,
-    Data DATE NOT NULL,
+    Jogo_ID INT,
+    Usuario_ID INT,
+    Data DATE,
     Estrelas INT NOT NULL,
-    Descricao TEXT,
+    Descricao VARCHAR(100),
     PRIMARY KEY (Jogo_ID, Usuario_ID, Data),
     FOREIGN KEY (Jogo_ID) REFERENCES Jogo(ID),
     FOREIGN KEY (Usuario_ID) REFERENCES Usuario(ID)
@@ -63,8 +64,8 @@ CREATE TABLE Player (
 
 -- Tabela Compartilha
 CREATE TABLE Compartilha (
-    Jogo_ID INT NOT NULL,
-    Dependente_ID INT NOT NULL,
+    Jogo_ID INT,
+    Dependente_ID INT,
     Dono_ID INT NOT NULL,
     PRIMARY KEY (Jogo_ID, Dependente_ID),
     FOREIGN KEY (Jogo_ID) REFERENCES Jogo(ID),
@@ -74,18 +75,18 @@ CREATE TABLE Compartilha (
 
 -- Tabela Tem
 CREATE TABLE Tem (
-    Usuario_ID INT NOT NULL,
-    Jogo_ID INT NOT NULL,
+    Usuario_ID INT,
+    Jogo_ID INT,
     PRIMARY KEY (Usuario_ID, Jogo_ID),
-    FOREIGN KEY (Usuario_ID) REFERENCES Usuario(ID),
     FOREIGN KEY (Jogo_ID) REFERENCES Jogo(ID)
+    FOREIGN KEY (Usuario_ID) REFERENCES Usuario(ID),
 );
 
 -- Tabela Conquistas
 CREATE TABLE Conquistas (
-    Usuario_ID INT NOT NULL,
-    Jogo_ID INT NOT NULL,
-    Conquista VARCHAR(100) NOT NULL,
+    Usuario_ID INT,
+    Jogo_ID INT,
+    Conquista VARCHAR(100),
     PRIMARY KEY (Usuario_ID, Jogo_ID, Conquista),
     FOREIGN KEY (Usuario_ID, Jogo_ID) REFERENCES Tem(Usuario_ID, Jogo_ID)
 );
@@ -104,9 +105,9 @@ CREATE TABLE Empresa (
 
 -- Tabela Desenvolve
 CREATE TABLE Desenvolve (
-    Usuario_ID INT NOT NULL,
-    Jogo_ID INT NOT NULL,
-    Empresa_ID INT NOT NULL,
+    Usuario_ID INT,
+    Jogo_ID INT,
+    Empresa_ID INT,
     PRIMARY KEY (Usuario_ID, Jogo_ID),
     FOREIGN KEY (Usuario_ID) REFERENCES Usuario(ID),
     FOREIGN KEY (Jogo_ID) REFERENCES Jogo(ID),
