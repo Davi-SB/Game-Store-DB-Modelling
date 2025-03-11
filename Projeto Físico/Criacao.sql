7d9f4970040c2cb92b1f7cdea0ec23d0b8b1e974
Davi <davibrilhante0102@gmail.com>
2025-03-10 21:27:23 -0300

    fix projeto fisico

--- Projeto Físico/Criacao.sql
+++ Projeto Físico/Criacao.sql
@@ -78,8 +78,8 @@ CREATE TABLE Tem (
     Usuario_ID INT,
     Jogo_ID INT,
     PRIMARY KEY (Usuario_ID, Jogo_ID),
-    FOREIGN KEY (Jogo_ID) REFERENCES Jogo(ID)
-    FOREIGN KEY (Usuario_ID) REFERENCES Usuario(ID),
+    FOREIGN KEY (Jogo_ID) REFERENCES Jogo(ID),
+    FOREIGN KEY (Usuario_ID) REFERENCES Usuario(ID)
 );
 
 -- Tabela Conquistas
